b293d7461e638845590a919d9688ea6c62431db3
unknown <dumoulinc268@gmail.com>
2019-06-23 19:19:09 +0200

    Initialisation

---
+++ power.py
@@ -0,0 +1,12 @@
+def power_of_two(x):
+    # for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.
+    #si x 
+    return x != 0 & ((x &(x-1)) == 0)
+
+
+assert power_of_two(0) == False
+assert power_of_two(1) == True
+assert power_of_two(2) == True
+assert power_of_two(5) == False
+assert power_of_two(6) == False
+assert power_of_two(4096) == True
